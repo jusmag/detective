eaf44e924d6a09cb8a0e43b9b1119054dac4db48
jusmag <65278847+jusmag@users.noreply.github.com>
2020-05-14 12:35:01 -0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,13 +1,23 @@
+var fs = require('fs');
+var path = require('path');
+
 module.exports = {
 	name: 'say',
-	description: 'Admin only; Will repeat what ever message is fed in',
+	description: 'Will repeat what ever message is fed in',
   args: true,
   usage: '<insert commands>',
   guildOnly: true,
-  adminOnly: true,
+  adminOnly: false,
 	execute(message, args) {
 		var data = "";
+	 	var blacklist = (fs.readFileSync(path.resolve(__dirname, './data/blacklist.txt'), 'utf-8')).split(",");
 		for(var i = 0; i < args.length; i++) {
+			for(var j = 0; j < blacklist.length; j++) {
+				if(args[i].toLowerCase() == blacklist[j].toLowerCase()) {
+					message.channel.send("Nice try. Found a blacklisted word: " + blacklist[j]);
+					return;
+				}
+			}
 			data += args[i];
 			data += " ";
 		}
