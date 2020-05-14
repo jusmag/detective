3c672adab7e22638ea42657bd98a6b122d9c4d0a
jusmag <65278847+jusmag@users.noreply.github.com>
2020-05-14 12:18:28 -0700

    Changed useless ping command to say command

---
+++ commands/say.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'say',
+	description: 'Admin only; Will repeat what ever message is fed in',
+  args: true,
+  usage: '<insert commands>',
+  guildOnly: true,
+  adminOnly: true,
+	execute(message, args) {
+		var data = "";
+		for(var i = 0; i < args.length; i++) {
+			data += args[i];	
+		}
+		message.channel.send(data);
+	},
+};
